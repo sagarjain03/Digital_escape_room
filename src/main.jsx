25cf69406300992a6e1c206816dc992629538b96
sagarjain03 <141217003+sagarjain03@users.noreply.github.com>
2025-03-08 15:58:22 +0530

    added reaction routes and clerk

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 import './index.css'
-import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter ,Route,Routes} from 'react-router-dom'
+import Reaction from './components/Reaction'
+import GuessAndG from './components/GuessAndG'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} >
+     <BrowserRouter>
+     <Routes>
+   <Route path="/" element={<Home/>} />
+   <Route path="/guess" element={<GuessAndG/>} />
+   <Route path="/reaction" element={<Reaction/>} />
+   <Route path="/login" element={<Login />} />
+   <Route path="/register" element={<Register />} />
+   </Routes>
+     </BrowserRouter>
+    </ClerkProvider>
+  </React.StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useUser, UserButton, SignInButton, SignUpButton } from "@clerk/clerk-react";
+
+const Home = () => {
+  const { isSignedIn } = useUser();
+
+  return (
+    <nav >
+      <Link to="/" className="text-xl font-bold">My App</Link>
+
+      <div>
+        {isSignedIn ? (
+          <>
+            <Link to="/reaction" className="mx-4">Let's Go</Link>
+           
+            <UserButton />
+          </>
+        ) : (
+          <>
+            <SignInButton />
+            <SignUpButton />
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Home;
