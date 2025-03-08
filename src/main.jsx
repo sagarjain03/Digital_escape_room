48771c6995161a3bd6ad4913f2dee2e1b95217f1
sagarjain03 <141217003+sagarjain03@users.noreply.github.com>
2025-03-08 16:14:36 +0530

    routes protected

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
-import { BrowserRouter ,Route,Routes} from 'react-router-dom'
-import Reaction from './components/Reaction'
-import GuessAndG from './components/GuessAndG'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Reaction from './components/Reaction';
+import GuessAndG from './components/GuessAndG';
+import ProtectedRoute from './components/ProtectedRoute'; // Protected Route import kiya
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error('Missing Publishable Key');
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} >
-     <BrowserRouter>
-     <Routes>
-   <Route path="/" element={<Home/>} />
-   <Route path="/guess" element={<GuessAndG/>} />
-   <Route path="/reaction" element={<Reaction/>} />
-   <Route path="/login" element={<Login />} />
-   <Route path="/register" element={<Register />} />
-   </Routes>
-     </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/guess"
+            element={
+              <ProtectedRoute>
+                <GuessAndG />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/reaction"
+            element={
+              <ProtectedRoute>
+                <Reaction />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </ClerkProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
